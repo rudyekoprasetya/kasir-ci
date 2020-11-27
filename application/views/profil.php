a8618a66407ac3af2fccb092cb07ba3134a03fab
Rudy Eko Prasetya <rudyekoprasetya@yahoo.co.id>
2020-11-27 17:24:01 +0700

    [FIX] Update grocery CRUD

---
+++ application/views/profil.php
@@ -0,0 +1,11 @@
+<div class="panel-heading">
+	<h3 class="panel-title"><i class="fa fa-table"></i> <?php echo $judul; ?></h3>
+</div>
+
+<div class="panel-body">
+  <div class="row">
+  	<div class="col-lg-12">
+  		
+  	</div>
+  </div>
+</div>
